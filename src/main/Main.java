59837d2d2dd65fb122d2942491f08a4322cbf8b0
Manuel Palomo Ramon <palomoraising@gmail.com>
2015-10-23 22:25:33 +0200

    Added several functionalities, encoding is functional but has a few bugs

---
+++ src/main/Main.java
@@ -0,0 +1,72 @@
+package main;
+
+import morse.MorseMap;
+import operation.MorseEncoder;
+import utils.Cons;
+
+/**
+ * Class main
+ * 
+ * @author Manuel Palomo <manuel_palomo@hotmail.es>
+ * 
+ *         <mode><system><pathToFile> Usage: This program accepts three
+ *         parameters. First parameter: -c (Code) -d (Decode) -p (Play)
+ * 
+ *         Second Parameter -i (International morse) -e (Extended morse)
+ * 
+ *         Third parameter: Path to the file to be encoded (ej: message.txt)
+ * 
+ *         Fourth parameter: Optional,name of the output file, default is
+ *         morseoutput.txt
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		if (args.length != 4) {
+			System.out.println("Incorrect number of arguments given. Usage: <mode><system><pathToFile>");
+			System.exit(-1);
+		} else {
+			// Map creation and morse system
+			String morseSystem = "";
+
+			switch (args[1]) {
+			case "-i":
+				morseSystem = Cons.INTERNATIONAL;
+				break;
+
+			case "-e":
+				morseSystem = Cons.EXTENDED;
+				break;
+
+			default:
+				System.out.println("Undefined morse system");
+				break;
+
+			}
+			MorseMap morse = new MorseMap(morseSystem);
+			switch (args[0]) {
+			case "-c": // Encode
+				MorseEncoder encoder = new MorseEncoder(args[2], morse);
+				String outputFileName = "morseoutput.txt";
+				if (args[3] != null) {
+					outputFileName = args[3];
+				}
+				encoder.encodeText(outputFileName);
+				System.exit(-1);
+				break;
+
+			case "-d":// Decode
+				break;
+
+			case "-p":// Play
+				break;
+
+			default:
+				System.out.println("Incorrect action given, Usage: <mode><system><pathToFile>");
+				System.exit(-1);
+				break;
+			}
+		}
+
+	}
+}
--- src/morse/MorseMap.java
+++ src/morse/MorseMap.java
@@ -4,14 +4,14 @@ import java.util.HashMap;
 
 import utils.Cons;
 
-/*
- * Class MorseDictionary
- * This is a data structure containing all the necessary methods for handling morse to text translation
- * It varies in content depending on what morse system is used(international or extended)
+/**
+ * @author Manuel Palomo <manuel_palomo@hotmail.es> Class MorseDictionary This
+ *         is a data structure containing all the necessary methods for handling
+ *         morse to text translation It varies in content depending on what
+ *         morse system is used(international or extended)
  * 
- * Extended has non-English variants
- * Dot(.) and dash (-)
- * @author Manuel Palomo <manuel_palomo@hotmail.es>
+ *         Extended has non-English variants Dot(.) and dash (-)
+ * 
  */
 public class MorseMap {
 	private HashMap<String, String> codes;
@@ -37,6 +37,7 @@ public class MorseMap {
 		codes = new HashMap<String, String>();
 		switch (morseSystem) {
 		case Cons.EXTENDED:
+			fillMapWithExtendedCode(codes);
 			break;
 		case Cons.INTERNATIONAL:
 			fillMapWithInternationalCode(codes);
@@ -46,7 +47,7 @@ public class MorseMap {
 	}
 
 	/**
-	 * Fills the map passed as parameter with the American morse codes
+	 * Fills the map passed as parameter with the International morse codes
 	 * 
 	 * @param map
 	 *            map to be filled
@@ -92,17 +93,86 @@ public class MorseMap {
 	}
 
 	/**
+	 * Fills the map passed as parameter with the Extended morse codes
+	 * 
+	 * @param map
+	 *            map to be filled
+	 */
+	public void fillMapWithExtendedCode(HashMap<String, String> map) {
+		map.put("A", ".-");
+		map.put("B", "-...");
+		map.put("C", "-.-.");
+		map.put("D", "-..");
+		map.put("E", ".");
+		map.put("F", "..-.");
+		map.put("G", "--.");
+		map.put("H", "....");
+		map.put("I", "..");
+		map.put("J", ".---");
+		map.put("K", "-.-");
+		map.put("L", ".-..");
+		map.put("M", "--");
+		map.put("N", "-.");
+		map.put("O", "---");
+		map.put("P", ".--.");
+		map.put("Q", "--.-");
+		map.put("R", ".-.");
+		map.put("S", "...");
+		map.put("T", "-");
+		map.put("U", "..-");
+		map.put("V", "...-");
+		map.put("W", ".--");
+		map.put("X", "-..-");
+		map.put("Y", "-.--");
+		map.put("Z", "--..");
+		map.put("1", ".----");
+		map.put("2", "..---");
+		map.put("3", "...--");
+		map.put("4", "....-");
+		map.put("5", ".....");
+		map.put("6", "-....");
+		map.put("7", "--...");
+		map.put("8", "---..");
+		map.put("9", "----.");
+		map.put("0", "-----");
+		map.put(".", ".-.-.-");
+		map.put(",", "--..--");
+		map.put("?", "..--..");
+		map.put("'", ".----.");
+		map.put("!", "-.-.--");
+		map.put("/", "-..-.");
+		map.put("(", "-.--.");
+		map.put(")", "-.--.-");
+		map.put("&", ".-...");
+		map.put(":", "---...");
+		map.put(";", "-.-.-.");
+		map.put("=", "-...-");
+		map.put("+", ".-.-.");
+		map.put("-", "-....-");
+		map.put("_", "..--.-");
+		map.put("\"ROM\"", ".-..-."); // "
+		map.put("$", "...-..-");
+		map.put("@", ".--.-.");
+		map.put("ç", "-.-..");
+		map.put("é", "..-..");
+		map.put("ó", "---.");
+		map.put("", "");
+		map.put("", "");
+
+	}
+
+	/**
 	 * Returns the code assigned to the letter provided, if there is no mapped
 	 * value, return an interrogant
 	 * 
-	 * @param letter
+	 * @param character
 	 * @return Morse code assigned to the letter provided
 	 */
-	public String getLetterCode(String letter) {
-		String code = codes.get(letter);
-		if (code.equals(null)) {
-			code = "?";
-		}
+	public String getLetterCode(char character) {
+		String code = codes.get(String.valueOf(character));
+//		if (code.equals(null)) {
+//			code = "?";
+//		}
 		return code;
 	}
 
